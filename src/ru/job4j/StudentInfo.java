587596a49991641d0edc9f680a6eedf71ae8d653
Azat Idrisov <azzzat72@yandex.ru>
2020-07-06 15:52:24 +0500

    3.3. Разбор первой программы. [#321139]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Azat Idrisov");
+        System.out.println("28.07.2020");
+    }
+}
