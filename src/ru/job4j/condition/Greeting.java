66ef8e1689100ddb77600c713cc72e5f1fa69ff8
Azat Idrisov <azzzat72@yandex.ru>
2020-07-07 19:57:29 +0500

    return to prev

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -6,7 +6,7 @@ public class Greeting {
         System.out.println(idea);
         idea += " But I am a newbie. ";
         int year = 2020;
-        idea += year;
+        idea +=year;
         System.out.println(idea);
     }
 }
