96450bec1f276d6e073d189d272a5c29ff5d80b2
Azat Idrisov <azzzat72@yandex.ru>
2020-07-09 22:14:48 +0500

    6.0.2. Заполнение массива. [#321104]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[5];
+        names[0] = "Alex Pushkin";
+        names[1] = "Dmitri Mendeleev";
+        names[2] = "Bjarne Stroustrup";
+        names[3] = "Azat Idrisov";
+        names[4] = "Petr Arsentev";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
