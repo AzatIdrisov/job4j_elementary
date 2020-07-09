6906829b55bffac22406e85ab03752c78f1dd7d2
Azat Idrisov <azzzat72@yandex.ru>
2020-07-09 13:15:54 +0500

    5.8. Именование классов и переменных [#321184]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,9 +3,9 @@ package ru.job4j.condition;
 public class SqArea {
 
     public static double square(int p, int k) {
-        double h = p/(2*(k+1));
-        double l = h*k;
-        double rsl = h*l;
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double rsl = h * l;
         return rsl;
     }
 
